bc63ff0b0ff7ed73365592208313e0043d0448c1
Carlos Albuquerque Carneiro <carloscarneiropmw@gmail.com>
2023-03-23 20:44:50 -0300

    groupcard

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,3 +1,6 @@
+import { GroupCard } from '@components/GroupCard';
+import { Header } from '@components/Header';
+import { Highlight } from '@components/Highlight';
 import { Container } from './styles';
 
 
@@ -5,7 +8,12 @@ export default function Groups() {
   return (
 
     <Container>
-    
+    <Header showBackButton/>
+    <Highlight
+    title='Turmas'
+    subtitle='Jogue com a sua turma'
+    />
+    <GroupCard title='Galera do ignite' onPress={()=>console.log("pressionou")} />
     </Container>
   );
 }
