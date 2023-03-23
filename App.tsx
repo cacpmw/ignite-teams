09c17608ad13c4897694027f77ab17368a818d25
Carlos Albuquerque Carneiro <carloscarneiropmw@gmail.com>
2023-03-23 14:19:17 -0300

    status bar

--- App.tsx
+++ App.tsx
@@ -1,4 +1,5 @@
-import { ThemeProvider } from "styled-components/native"
+import { ThemeProvider } from "styled-components/native";
+import { StatusBar} from "react-native";
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/roboto";
 import Groups from "@screens/Groups";
 import theme from "@theme/index";
@@ -12,7 +13,12 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      {!fontsLoaded ? <Groups /> : <Loading />}
+       <StatusBar
+       barStyle="light-content"
+       backgroundColor="transparent"
+       translucent
+      />
+      {fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
   );
 }
