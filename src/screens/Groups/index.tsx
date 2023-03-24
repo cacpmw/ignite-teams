bf86a16b489053de2df603663b95994f5a7f7d8c
Carlos Albuquerque Carneiro <carloscarneiropmw@gmail.com>
2023-03-23 21:11:34 -0300

    List empty

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,10 +1,14 @@
 import { GroupCard } from '@components/GroupCard';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
+import { useState } from 'react';
+import { FlatList } from 'react-native';
 import { Container } from './styles';
+import { ListEmpty } from '@components/ListEmpty';
 
 
 export default function Groups() {
+  const  [groups, setGroups] = useState<string[]>(["Turma 1","Amigos"]);
   return (
 
     <Container>
@@ -13,7 +17,20 @@ export default function Groups() {
     title='Turmas'
     subtitle='Jogue com a sua turma'
     />
-    <GroupCard title='Galera do ignite' onPress={()=>console.log("pressionou")} />
+
+    <FlatList
+      showsVerticalScrollIndicator={false}
+      data={groups}
+      keyExtractor={(item, index) => String(index)}
+      renderItem={({ item }) =>(
+        <GroupCard 
+          title={item} 
+        />
+
+      )}
+      contentContainerStyle={groups.length ===0 &&{flex: 1}}
+      ListEmptyComponent={<ListEmpty message='Nenhuma turma' />}
+    />
     </Container>
   );
 }
